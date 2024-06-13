34578ed397aa523d6673e87ab0afd8bd3defd32b
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 15:04:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab8_kt/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lab8_kt
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        textView = findViewById(R.id.textView1)
+    }
+}
